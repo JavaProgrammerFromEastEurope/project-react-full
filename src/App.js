0e96da09d13e006867c86216e8de09149bfd0ca4
vm <gordonfree.com@gmail.com>
2022-11-15 14:45:25 +0300

    prev_state

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import Navbar from "./components/UI/Navbar/Navbar";
+import { AuthContext } from "./context";
+import { Login, Posts, PostIdPage, Error, About } from "./pages";
+
+const App = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      setIsAuth(true);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        setIsAuth,
+        isLoading
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostIdPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/error" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
